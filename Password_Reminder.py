bf7de348982f500aedf604b323031ed0abbb0a1c
zehraturan <zehraturangr@gmail.com>
2021-04-18 00:20:09 +0300

    First commit

---
+++ Password_Reminder.py
@@ -0,0 +1,6 @@
+name = "Zehra"
+user_name = input("Please enter your name: ").strip().title()
+if user_name == name :
+    print(f"Hello, {user_name} The password is : W@12")
+else :
+    print(f"Hello, {user_name}! See you later")
